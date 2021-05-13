bfc758c7b3c4c30350f203b9bc280d61fcac5adc
mobile-dev-hybrid <cutecate2019118@gmail.com>
2021-05-13 21:35:44 +0300

    initial commit

---
+++ src/config/constants/farms.ts
@@ -0,0 +1,60 @@
+import tokens from './tokens'
+import { FarmConfig } from './types'
+
+const farms: FarmConfig[] = [
+  {
+    pid: 251,
+    lpSymbol: 'SOY-CLO LP',
+    lpAddresses: {
+      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
+      56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
+    },
+    token: tokens.cake,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 252,
+    lpSymbol: 'SOY-BUSDT LP',
+    lpAddresses: {
+      97: '',
+      56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+    },
+    token: tokens.busd,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 364,
+    lpSymbol: 'CLO-BUSDT LP',
+    lpAddresses: {
+      97: '',
+      56: '0x8271d7eafeeb8f24d7c9fe1acce2ae20611972e5',
+    },
+    token: tokens.lmt,
+    quoteToken: tokens.wbnb,
+  },
+  /**
+   * All farms below here are from v1 and are to be set to 0x
+   */
+  {
+    pid: 1,
+    lpSymbol: 'SOY-CLO LP',
+    lpAddresses: {
+      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
+      56: '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
+    },
+    token: tokens.cake,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 2,
+    lpSymbol: 'SOY-BUSDT LP',
+    lpAddresses: {
+      97: '0x2f7682b64b88149ba3250aee32db712964de5fa9',
+      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+    },
+    token: tokens.busd,
+    quoteToken: tokens.wbnb,
+  },
+]
+
+export default farms
